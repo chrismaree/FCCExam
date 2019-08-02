4cafcbdb6353147c76e8d1a686f2fb2bfbca6aee
chrismaree <christopher.maree@gmail.com>
2019-08-02 23:39:32 +0200

    Updated script and config

--- Question 2.1/truffle-config.js
+++ Question 2.1/truffle-config.js
@@ -1,15 +1,9 @@
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
-
 module.exports = {
-  // networks: {
-  //   development: {
-  //     host: "127.0.0.1", // Localhost (default: none)
-  //     port: 8545, // Standard Ethereum port (default: none)
-  //     network_id: "*" // Any network (default: none)
-  //   }
-  // }
+  networks: {
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*" // Any network (default: none)
+    }
+  }
 };
