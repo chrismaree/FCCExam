eb3c55fb891c2e77f29385f7acbadcfc4510f4a6
chrismaree <christopher.maree@gmail.com>
2019-08-02 23:53:30 +0200

    Added small update to testing script

--- testAllContracts.sh
+++ testAllContracts.sh
@@ -2,7 +2,7 @@
 
 echo "Running tests on all three questions..."
 echo "Starting ganache CLI local dev blockchain"
-ganache-cli --gasLimit <yourLimit> 2> /dev/null 1> /dev/null &
+ganache-cli 2> /dev/null 1> /dev/null &
 sleep 3
 echo "***Testing Question 2.1***"
 cd Question\ 2.1/
@@ -14,3 +14,4 @@ echo "***Testing Question 2.3***"
 cd ../Question\ 2.3/
 truffle test
 kill -9 $(lsof -t -i:8545)
+echo "Closed Ganache CLI and finished running all tests"
